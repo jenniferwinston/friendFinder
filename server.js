596ba3b07af372918fe015d5322131e41e0f0601
Jennifer Winston <winston.jennifer9@gmail.com>
2016-09-20 14:13:19 -0400

    added html and styling

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
- 
- var express = require('express');
- var bodyParser = require('body-parser');
- var path = require('path');
+// Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
 
- var app = express();
- var PORT = process.envPORT || 3000;
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
- 
+var app = express();
+var PORT = process.envPORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+require('./routing/api-routes.js')(app);
+require('./routing/html-routes.js')(app);
+
+app.listen(PORT, function(){
+	console.log("App lsitening on PORT: "+ PORT);
+});
