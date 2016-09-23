f2ed00d611eb7b5d650cc6a0e82524f3ea15ae0a
Jennifer Winston <winston.jennifer9@gmail.com>
2016-09-23 14:51:23 -0400

    styling edits

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+// object for user data
+
+var friends = [
+	{
+		name: "Jane",
+		photo: "http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/jane-porter-tarzan-74.8.jpg",
+		scores:[3, 3, 1, 3, 2, 1, 2, 1, 4, 1]
+	}
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
-require('./routing/api-routes.js')(app);
-require('./routing/html-routes.js')(app);
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function(){
 	console.log("App lsitening on PORT: "+ PORT);
